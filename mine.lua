2e29641b0f43eaff4c606f1d4bfb48a3df2ed93f
Klara Robinson <60410893+Rinbadagin@users.noreply.github.com>
2025-08-05 14:26:07 +1200

    Update mine.lua to accept modded ores with different names

--- mine.lua
+++ mine.lua
@@ -1244,7 +1244,7 @@ end
 
 local function isOre(inspectFunction)
 	local success, data = inspectFunction()
-	if success and helper.stringEndsWith(data.name, "_ore") then
+	if success and (string.match(data.name, "ore") or string.match(data.name, "resource") then
 		return true
 	end
 	return false
